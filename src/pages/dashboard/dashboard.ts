c5c3b4f961c0af54f423a3e1ba9831a14eb4ad81
awstin17 <austinwiese.acw@gmail.com>
2019-03-29 16:28:54 -0700

    Cleaned up dashboard files' code

--- src/pages/dashboard/dashboard.ts
+++ src/pages/dashboard/dashboard.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams, ToastController } from 'ionic-angular';
+import { NavController } from 'ionic-angular';
 import { TimelinePage } from '../timeline/timeline';
-import { Storage } from '@ionic/storage'
 import { ChartProvider } from '../../providers/chart/chart';
-import { UserProvider } from '../../providers/user/user';
-
 
 @Component({
   selector: 'page-dashboard',
@@ -12,31 +9,10 @@ import { UserProvider } from '../../providers/user/user';
 })
 export class DashboardPage {
 
-  name: any
-  date: any
-
-  constructor(public navCtrl: NavController, public navParams: NavParams,public chartProvider: ChartProvider, private toastCtrl: ToastController, private storage: Storage, public user: UserProvider) { }
-
-  // ionViewWillLoad() {
-  //   this.storage.get('userInfo').then((val) => {
-  //     this.name = val ? `${val.firstName} ${val.lastName}` : '';
-  //   })
-  //   this.storage.get('chartData').then((val) => {
-  //     this.date = val ? val.Date : '';
-  //   }).then(() => this.lastDate())
-  // }
+  constructor(public navCtrl: NavController, public chartProvider: ChartProvider) { }
 
   toTimeline() {
     this.navCtrl.setRoot(TimelinePage);
   }
 
-  // lastDate() {
-  //   let toast = this.toastCtrl.create({
-  //     message: `Your last assessment was ${this.date}`,
-  //     duration: 2500,
-  //     position: 'middle'
-  //   });
-
-  //   toast.present();
-  // }
 }
