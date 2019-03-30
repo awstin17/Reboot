5887b05003cbfdd655d2f7ca7db70c3c45f6d9c2
awstin17 <austinwiese.acw@gmail.com>
2019-03-29 17:21:37 -0700

    Cleaned up user provider code

--- src/providers/user/user.ts
+++ src/providers/user/user.ts
@@ -1,45 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-// import  {  ENV  }  from  '@app/env';
-
-/* 
-  Generated class for the UserProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class UserProvider {
 
   requestUrl: string = "https://reboot-ssf.herokuapp.com/api"
-  userName: any = "Maurice";
 
-  constructor(public http: HttpClient) {
-    console.log('Hello UserProvider Provider');
-  }
+  constructor(public http: HttpClient) { }
 
   sendReg(user) {
-    console.log('sendReg() runs', user)
     return this.http.post(this.requestUrl + '/appUsers', user)
   }
+
   //update data from wizard page and patch user model
   updateUserModel(data: any, id) {
-    console.log(data, "#1-updateUserModel") 
     return this.http.patch(this.requestUrl + '/appUsers/' + id , data)
   }
 
   login(creds) {
     return this.http.post(this.requestUrl + '/appUsers/login', creds);
   }
-
-  // logoutUser() {
-  //   window.sessionStorage.clear();
-
-  // }
   
-  getUser(id) {
-    return this.http.get(this.requestUrl + '/appUsers/' + id)
-
-  }
 }
